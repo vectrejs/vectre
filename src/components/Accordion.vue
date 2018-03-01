91f1844580cce1f68aacf058ca538ab3872991b7
vatson <brainreflex@gmail.com>
2018-03-01 17:02:24 +0200

    Accordion improvements

--- src/components/Accordion.vue
+++ src/components/Accordion.vue
@@ -1,30 +1,63 @@
 <template>
-  <div class="accordion">
-    <input type="checkbox" hidden :id="'accordion-' + index" name="accordion-checkbox" :checked="isSelected()">
-    <label class="accordion-header c-hand" :for="'accordion-' + index">
-      <slot name="header"></slot>
-    </label>
-    <div class="accordion-body">
-      <slot name="body"></slot>
-    </div>
-  </div>  
+  <div class="accordion-container">
+    <div class="accordion" v-for="(value, key, index) of items" :key="index">
+      
+      <input :type="type" hidden :id="uid(index)" :name="id" :checked="isSelected(key, index)">
+      
+      <label class="accordion-header c-hand" :for="uid(index)">
+        <slot name="header" :item="{value, key, index}"></slot>
+      </label>
+      
+      <div class="accordion-body">
+        <slot name="body" :item="{value, key, index}"></slot>
+      </div>
+
+    </div>  
+  </div>
 </template>
 
 <script lang="ts">
-import vue from 'vue'
+import vue, { CreateElement } from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
+import { VNode } from 'vue/types/vnode';
 
 @Component
 export default class extends vue {
-  @Prop()
-  private checked: number;
+  @Prop({ required: true, type: [Object, Array] })
+  private items: object[] | object;
 
-  @Prop()
-  private index: number;
+  @Prop([String, Number, Array])
+  private checked: string | number | string[] | number[];
 
-  public isSelected(): boolean {
+  @Prop([String])
+  private name: string;
 
-    return this.checked === this.index
+  @Prop([Boolean])
+  private multiple: boolean;
+
+  private _id: string;
+
+  public get type(): string {
+    return this.multiple ? 'checkbox' : 'radio';
+  }
+
+  public uid(index: number): string {
+    return this.id + '-' + index;
+  }
+
+  public isSelected(key: string, index: number): boolean {
+    if (Array.isArray(this.checked) === false) {
+      return !!this.checked && (this.checked == key || this.checked == index);  
+    }
+
+    if ((<number[]>this.checked).indexOf(index) !== -1) {
+      return true;
+    }
+    return (<string[]>this.checked).indexOf(key) !== -1;
+  }
+
+  public get id(): string {
+    return this.name ? this.name : 'accordion-' + Math.round(Math.random() * 1000)
   }
 }
 </script>
