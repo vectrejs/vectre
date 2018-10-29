7253ff033632a9226d1fe068361683c9215245e9
vatson <brainreflex@gmail.com>
2018-10-29 16:07:48 +0200

    Adds Loading directive

--- src/directive/index.ts
+++ src/directive/index.ts
@@ -1,14 +1,17 @@
 import { Badge } from './Badge';
+import { Loading } from './Loading';
 import { Tooltip, TooltipSide } from './Tooltip';
 
 // Directives
 export default {
   Badge,
+  Loading,
   Tooltip,
 };
 
 export {
   Badge,
+  Loading,
   Tooltip,
   TooltipSide,
 };
