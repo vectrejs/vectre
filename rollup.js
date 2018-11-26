ed92a4e3ff7edb11367c91ff30f4827b72729615
vatson <brainreflex@gmail.com>
2018-11-26 21:06:55 +0200

    Replace buble with babel Adds chokidar to watch script Final version of build script

--- rollup.js
+++ rollup.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 
-const buble = require('rollup-plugin-buble');
+const babel = require('rollup-plugin-babel');
 const commonjs = require('rollup-plugin-commonjs');
-const css = require('rollup-plugin-css-only');
 const nodeResolve = require('rollup-plugin-node-resolve');
 const typescript = require('rollup-plugin-typescript2');
 const { default: vue } = require('rollup-plugin-vue');
@@ -12,93 +11,90 @@ const { resolve } = require('path');
 
 const argv = require('minimist')(process.argv.slice(2));
 
-
 const source = resolve(__dirname, 'src/');
 const dest = resolve(__dirname, 'dist/');
 
+const package = require('./package.json');
+const deps = Object.keys({...package.devDependencies, ...package.dependencies})
+const external = deps.filter(dep => dep !== 'vue-tsx-helper' && dep !=='vue-property-decorator')
+
 const input = {
-  input: resolve(source, 'main.ts'),
-  plugins: [
-    vue({ css: true }),
-    typescript({ useTsconfigDeclarationDir: false }),
-    buble({
-      jsx: 'h',
-      objectAssign: 'Object.assign',
-      transforms: {
-        forOf: false,
-      },
-    }),
-    nodeResolve(),
-    commonjs(),
-    // terser(),
-  ],
-  external: [
-    'vue',
-    'Vue',
-  ],
+  external,
+	input: resolve(source, 'main.ts'),
+	plugins: [
+		vue({ css: true }),
+		typescript({ useTsconfigDeclarationDir: false }),
+		nodeResolve(),
+		commonjs(),
+		babel({
+			runtimeHelpers: true,
+			exclude: 'node_modules/**',
+			extensions: [ '.js', '.jsx', '.tsx', '.ts' ]
+		})
+	],
 };
+const minInput = { ...input, plugins: [ ...input.plugins, terser() ] };
 
-
-const globals = {
-  vue: 'Vue',
-};
+const outs = [
+	{
+		file: 'vectre.esm.js',
+		format: 'esm'
+	},
+	{
+		file: 'vectre.cjs.js',
+		format: 'cjs'
+	},
+	{
+		globals: { vue: 'Vue' },
+		format: 'umd',
+		name: 'vectre',
+		file: 'vectre.js'
+	}
+];
 
 async function build() {
-  const bundle = await rollup(input);
+	const bundle = await rollup(input);
+	const minBundle = await rollup(minInput);
 
-  await bundle.write({
-    format: 'esm',
-    file: resolve(dest, 'vectre.esm.js'),
-    exports: 'named',
-  });
-  await bundle.write({
-    format: 'cjs',
-    file: resolve(dest, 'vectre.cjs.js'),
-    exports: 'named',
-  });
-  await bundle.write({
-    globals,
-    format: 'umd',
-    name: 'vectre',
-    file: resolve(dest, 'vectre.js'),
-    exports: 'named',
-  });
+  for(const out of outs) {
+    await bundle.write({ ...out, file: resolve(dest, out.file) });
+    await minBundle.write({ ...out, file: resolve(dest, out.file.replace('.js', '.min.js')) });
+  }
 }
 
 function wtch() {
-  const watcher = watch({
-    ...input,
-    output: {
-      format: 'cjs',
-      file: resolve(dest, 'vectre.cjs.js'),
-      exports: 'named',
-    },
-    watch: {
-      inclued: 'src/**',
-      chokidar: true,
-    }
-  });
+	const watcher = watch({
+		...input,
+		output: {
+			format: 'cjs',
+			file: resolve(dest, 'vectre.cjs.js')
+		},
+		watch: {
+			inclued: 'src/**',
+			chokidar: true
+		}
+	});
 
-  watcher.on('event', ({ code, error }) => {
-    switch (code) {
-      case 'START':
-        console.log('In progress');
-        break;
-      case 'FATAL':
-      case 'ERROR':
-        console.error(error);
-        break;
-      case 'END':
-        console.log('Completed');
-    }
-  })
+	watcher.on('event', ({ code, error }) => {
+		switch (code) {
+			case 'START':
+				console.log('In progress');
+				break;
+			case 'FATAL':
+			case 'ERROR':
+				console.error(error);
+				break;
+			case 'END':
+				console.log('Completed');
+		}
+	});
 }
 
 if (argv.watch) {
-  wtch();
+	wtch();
 } else {
-  build().catch(err => {
-    console.error(err);
-    process.exitCode = 1;
-  });
+	build().catch((err) => {
+		console.error(err);
+		process.exitCode = 1;
+	});
 }
