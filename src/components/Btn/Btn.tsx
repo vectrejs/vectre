9fad2799db699b91011ef46f177aea62e11a6e73
Vadim Tiukov <dev.vatson@gmail.com>
2020-10-06 18:23:33 +0300

    feat: add `htmlTag` prop to `Btn` component to render button as an ordinary link.
    The property can take only 2 options: "a" and "button"

--- src/components/Btn/Btn.tsx
+++ src/components/Btn/Btn.tsx
@@ -7,8 +7,9 @@ import { BtnState, BtnStates } from './State';
 import { IconType } from '../Icon';
 import { Icon } from '../Icon/Icon';
 import { BtnEvents } from './Events';
+import { mergeCss } from '../../utils/css';
 
-export const Btn = tsx.componentFactoryOf<BtnEvents>().create({
+export const Btn = /*#__PURE__*/ tsx.componentFactoryOf<BtnEvents>().create({
   name: 'Btn',
   functional: true,
   props: {
@@ -20,27 +21,29 @@ export const Btn = tsx.componentFactoryOf<BtnEvents>().create({
     left: { type: Boolean },
     circle: { type: Boolean },
     action: { type: Boolean },
+    htmlTag: {
+      type: String as () => 'a' | 'button',
+      validator: (tag: 'a' | 'button'): boolean => ['a', 'button'].includes(tag),
+    },
   },
   render(h: CreateElement, { props, data, slots }): VNode {
-    const cssClass = [
-      'btn',
+    const cssClass = mergeCss(data, 'btn', [
       BtnTypes[props.type as BtnType] || props.type,
       BtnSizes[props.size] || props.size,
       BtnStates[props.state] || props.state,
       props.action && props.circle && 's-circle',
       props.action && 'btn-action',
-    ];
+    ]);
 
     const leftIcon = props.icon && props.left ? <Icon name={props.icon} class="left" /> : '';
     const rightIcon = props.icon && !props.left ? <Icon name={props.icon} /> : '';
     const content = !props.action && slots().default;
+    const htmlTag = ['a', 'button'].includes(props.htmlTag) ? props.htmlTag : 'button';
 
-    return (
-      <button class={cssClass} {...data}>
-        {leftIcon}
-        {content}
-        {rightIcon}
-      </button>
-    );
+    return h(htmlTag, { ...data, class: cssClass, attrs: { tabindex: props.tabindex, ...data.attrs } }, [
+      leftIcon,
+      content,
+      rightIcon,
+    ]);
   },
 });
